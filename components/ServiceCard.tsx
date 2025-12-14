9b6b96616ee66d1e92fd757c8675024aaac7a78c
jijuw <154947283+Jejeyyyy@users.noreply.github.com>
2025-12-14 23:58:18 +0700

    Create ServiceCard component

---
+++ components/ServiceCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServiceCard: React.FC = () => {
+  return (
+    <div className="service-card">
+      <h3>Service Card</h3>
+      <p>This is the ServiceCard component.</p>
+    </div>
+  );
+};
+
+export default ServiceCard;
